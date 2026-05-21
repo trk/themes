9cacfe96951f4e9955be6c0e97e4631b4b10e11f
Iskender TOTOGLU <totoglu@gmail.com>
2026-05-21 23:48:23 +0300

    fix: remove external bunny fonts

--- theme-moni/resources/views/layouts/app.blade.php
+++ theme-moni/resources/views/layouts/app.blade.php
@@ -22,10 +22,6 @@
         <link rel="icon" href="{{ Storage::disk(cms_media_disk())->url($favicon) }}">
     @endif
 
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700&display=swap" rel="stylesheet" />
-
     <!-- CMS Core Runtime (shared Alpine components) -->
     @tcmsCoreJs
     <!-- Theme Assets -->
